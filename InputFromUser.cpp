6748723f35482a0acb8a9221d5915c11ea80548c
ZeroToPro <96923915+DivyaPundhir@users.noreply.github.com>
2022-01-02 00:48:23 +0530

    Add files via upload

---
+++ InputFromUser.cpp
@@ -0,0 +1,13 @@
+//Program to take input from user
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int number;
+    cout << "Enter a number: "<<endl;
+    cin>>number;
+    cout<<"You entered: "<<number;
+    return 0;
+}
