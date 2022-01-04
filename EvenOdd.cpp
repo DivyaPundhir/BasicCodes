413b2d98d4ef8fec40f6278fe8044010e831629e
ZeroToPro <96923915+DivyaPundhir@users.noreply.github.com>
2022-01-04 21:15:08 +0530

    Add files via upload

---
+++ EvenOdd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number=0;
+    cout << "Enter a number to check even or odd";
+    cin>>number;
+    if(number%2==0)
+    cout<<"Even Number";
+    else
+    cout<<"Odd Number";
+}
