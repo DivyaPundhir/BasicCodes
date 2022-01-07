6ca93d55573ced17703a394cecd20d81f9af0469
ZeroToPro <96923915+DivyaPundhir@users.noreply.github.com>
2022-01-07 23:14:53 +0530

    Add files via upload

---
+++ PrintNNaturalNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+  int target;
+    cout << "Enter the target number";
+    cin>>target;
+    for(int i=1;i<=target;i++)
+    cout<<i<<" ";
+}
